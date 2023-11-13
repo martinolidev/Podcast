85deec3ffbfff2c7f1a172c22e2f8b8b20a4b97a
Martin Martinez <mtz.martin@proton.me>
2023-11-13 16:59:13 -0600

    New file, I don't wanna repeat the past process. I learn the lesson ok?

---
+++ Podcast/History.swift
@@ -0,0 +1,18 @@
+//
+//  History.swift
+//  Podcast
+//
+//  Created by Martin Martinez on 13/11/23.
+//
+
+import SwiftUI
+
+struct History: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    History()
+}
