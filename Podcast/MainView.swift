0aeca61db37a790078b078730baa2fd980f9da78
Martin Martinez <mtz.martin@proton.me>
2023-11-13 16:38:24 -0600

    I created this new card, it looks great on both dark and light mode

--- Podcast/MainView.swift
+++ Podcast/MainView.swift
@@ -11,6 +11,7 @@ struct MainView: View {
     var body: some View {
         VStack {
             TopBar()
+            HorizontalCard()
         }
         .padding()
         
@@ -62,6 +63,52 @@ struct TopBar: View {
             }
         }
 
+    }
+}
+
+struct HorizontalCard: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .frame(width: 360, height: 200)
+            .foregroundColor(.indigo)
+            .overlay(
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("ENJOY TOP PODCAST")
+                            
+                        Text("Listen Favourite\nPodcast")
+                            .padding(.top)
+                            .bold()
+                            .font(.title2)
+                        
+                        Button {
+                            //
+                        } label: {
+                            RoundedRectangle(cornerRadius: 25.0)
+                                .frame(width: 110, height: 30)
+                                .foregroundColor(Color(red: 144/255, green: 238/255, blue: 144/255))
+                                .overlay(
+                                    HStack{
+                                        Image(systemName: "play.circle.fill")
+                                            .foregroundColor(.black)
+                                            .font(.title3)
+                                        Text("Play Now")
+                                            .foregroundColor(.black)
+                                            .font(.footnote)
+                                            .bold()
+                                    }
+                                )
+                        }
+                        .shadow(radius: 10)
+                            
+                    }.foregroundColor(.white)
+                    
+                    Image("fondo")
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .scaledToFit()
+                }
+            )
     }
 }
 
