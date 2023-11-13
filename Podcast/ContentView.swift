5cbab9c18119e7a18ef6702cce20a6638a2b9aec
Martin Martinez <mtz.martin@proton.me>
2023-11-13 15:46:40 -0600

    Initial Commit

---
+++ Podcast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Podcast
+//
+//  Created by Martin Martinez on 13/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Podcast/PodcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastApp.swift
+//  Podcast
+//
+//  Created by Martin Martinez on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PodcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
