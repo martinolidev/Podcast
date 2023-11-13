75d2c73f9488d9d473728191c64b8abefb8a0bbb
Martin Martinez <mtz.martin@proton.me>
2023-11-13 16:01:47 -0600

    I think this way the code will be easy to fix if it has problems, since im separating the UI pieces into different structures

--- Podcast/MainView.swift
+++ Podcast/MainView.swift
@@ -9,52 +9,59 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        
         VStack {
-            //
-            HStack {
-                Circle()
-                    .frame(width: 50)
-                    .foregroundColor(.indigo)
-                    .overlay(
-                        Image(systemName: "mic")
-                            .foregroundColor(.white)
-                    )
-                Text("Podcast")
-                    .font(.title3)
-                    .bold()
-                
-                Spacer()
-                
-                Button {
-                    //
-                } label: {
-                    Circle()
-                        .frame(width: 50)
-                        .foregroundColor(Color(red: 220/255, green: 220/255, blue: 220/255))
-                        .overlay(
-                            Image(systemName: "magnifyingglass")
-                                .foregroundColor(.black)
-                                .font(.title2)
-                        )
-                }
-                
-                Button {
-                    //
-                } label: {
-                    Circle()
-                        .frame(width: 50)
-                        .foregroundColor(Color(red: 220/255, green: 220/255, blue: 220/255))
-                        .overlay(
-                            Image(systemName: "bell.badge")
-                                .foregroundColor(.black)
-                                .font(.title2)
-                        )
-                }
-            }
+            TopBar()
         }
         .padding()
         
+    }
+}
+
+//I still don't know if this is a good idea :D
+struct TopBar: View {
+    var body: some View {
+        // top bar
+        HStack {
+            Circle()
+                .frame(width: 50)
+                .foregroundColor(.indigo)
+                .overlay(
+                    Image(systemName: "mic")
+                        .foregroundColor(.white)
+                )
+            Text("Podcast")
+                .font(.title3)
+                .bold()
+            
+            Spacer()
+            
+            Button {
+                //
+            } label: {
+                Circle()
+                    .frame(width: 50)
+                    .foregroundColor(Color(red: 220/255, green: 220/255, blue: 220/255))
+                    .overlay(
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.black)
+                            .font(.title2)
+                    )
+            }
+            
+            Button {
+                //
+            } label: {
+                Circle()
+                    .frame(width: 50)
+                    .foregroundColor(Color(red: 220/255, green: 220/255, blue: 220/255))
+                    .overlay(
+                        Image(systemName: "bell.badge")
+                            .foregroundColor(.black)
+                            .font(.title2)
+                    )
+            }
+        }
+
     }
 }
 
