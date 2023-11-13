aabe41e57ca93c7e8b4c64a06157919fd83804fb
Martin Martinez <mtz.martin@proton.me>
2023-11-13 17:29:39 -0600

    Recommendations view

---
+++ Podcast/Recommended.swift
@@ -0,0 +1,18 @@
+//
+//  Recommended.swift
+//  Podcast
+//
+//  Created by Martin Martinez on 13/11/23.
+//
+
+import SwiftUI
+
+struct Recommended: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Recommended()
+}
