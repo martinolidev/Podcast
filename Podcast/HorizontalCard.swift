b7d0ebe04d41b661443a3d18e0739e937b57dac0
Martin Martinez <mtz.martin@proton.me>
2023-11-13 16:55:38 -0600

    New file for the horizontal cards

---
+++ Podcast/HorizontalCard.swift
@@ -0,0 +1,18 @@
+//
+//  HorizontalCard.swift
+//  Podcast
+//
+//  Created by Martin Martinez on 13/11/23.
+//
+
+import SwiftUI
+
+struct HorizontalCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HorizontalCard()
+}
