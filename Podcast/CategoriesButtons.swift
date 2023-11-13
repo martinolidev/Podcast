1a20687d53fd3545a8c7ce7e5e184923bbd84a0d
Martin Martinez <mtz.martin@proton.me>
2023-11-13 16:57:19 -0600

    New file for the category buttons

---
+++ Podcast/CategoriesButtons.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesButtons.swift
+//  Podcast
+//
+//  Created by Martin Martinez on 13/11/23.
+//
+
+import SwiftUI
+
+struct CategoriesButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoriesButtons()
+}
