316de870a83ba08943778125039b6f6798bdb138
Martin Martinez <mtz.martin@proton.me>
2023-11-13 17:31:29 -0600

    Recommendations created, i need the navigation bar

--- Podcast/MainView.swift
+++ Podcast/MainView.swift
@@ -14,6 +14,7 @@ struct MainView: View {
             HorizontalCard()
             CategoriesButtons()
             History()
+            Recommended()
         }
         .padding()
         
--- Podcast/Recommended.swift
+++ Podcast/Recommended.swift
@@ -9,10 +9,53 @@ import SwiftUI
 
 struct Recommended: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Text("Recommendations")
+                .font(.title3)
+                .bold()
+            
+            Spacer()
+            
+            Button("View All") {
+                
+            }
+            .buttonStyle(.borderless)
+            .foregroundColor(.indigo)
+            .font(.title3)
+        }
+        .padding(.top)
+        
+        RoundedRectangle(cornerRadius: 25.0)
+            .foregroundColor(.clear)
+            .frame(height: 150)
+            .overlay(
+                HStack {
+                    Image("ep2")
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(25)
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .leading) {
+                        Text("EP : 24 | Profitable Strategies")
+                            .bold()
+                        
+                        Text("Martin Martinez")
+                            .bold()
+                            .font(.system(size: 15))
+                            .padding(.top)
+                        HStack {
+                            ProgressView(value: 0.2)
+                                .progressViewStyle(LinearProgressViewStyle(tint: .indigo))
+                            Text("80%")
+                        }
+                    }
+                }
+            )
     }
 }
 
 #Preview {
-    Recommended()
+    MainView()
 }
