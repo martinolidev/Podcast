56b394d813a34c87e99054ce8e57f37ed4125945
Martin Martinez <mtz.martin@proton.me>
2023-11-13 16:58:02 -0600

    File for the buttons, this is better

--- Podcast/CategoriesButtons.swift
+++ Podcast/CategoriesButtons.swift
@@ -7,9 +7,95 @@
 
 import SwiftUI
 
+
 struct CategoriesButtons: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Category")
+                    .font(.title3)
+                    .bold()
+                Spacer()
+                Button("See All") {
+                    
+                }
+                    .buttonStyle(.borderless)
+                    .foregroundColor(.indigo)
+                    .font(.title3)
+            }
+            
+            HStack(spacing: 35) {
+                
+                Button {
+                    //
+                } label: {
+                    VStack {
+                        Circle()
+                            .frame(width: 50)
+                            .foregroundColor(Color(red: 220/255, green: 220/255, blue: 220/255))
+                            .overlay(
+                                Image(systemName: "gamecontroller.fill")
+                                    .foregroundColor(.indigo)
+                            )
+                        Text("Gaming")
+                            .foregroundColor(.black)
+                            .bold()
+                    }
+                }
+                
+                Button {
+                    //
+                } label: {
+                    VStack {
+                        Circle()
+                            .frame(width: 50)
+                            .foregroundColor(Color(red: 220/255, green: 220/255, blue: 220/255))
+                            .overlay(
+                                Image(systemName: "paintpalette.fill")
+                                    .foregroundColor(.indigo)
+                            )
+                        Text("Art")
+                            .foregroundColor(.black)
+                            .bold()
+                    }
+                }
+                
+                Button {
+                    //
+                } label: {
+                    VStack {
+                        Circle()
+                            .frame(width: 50)
+                            .foregroundColor(Color(red: 220/255, green: 220/255, blue: 220/255))
+                            .overlay(
+                                Image(systemName: "sunglasses.fill")
+                                    .foregroundColor(.indigo)
+                            )
+                        Text("Fashion")
+                            .foregroundColor(.black)
+                            .bold()
+                    }
+                }
+                
+                Button {
+                    //
+                } label: {
+                    VStack {
+                        Circle()
+                            .frame(width: 50)
+                            .foregroundColor(Color(red: 220/255, green: 220/255, blue: 220/255))
+                            .overlay(
+                                Image(systemName: "airplane.departure")
+                                    .foregroundColor(.indigo)
+                            )
+                        Text("Travel")
+                            .foregroundColor(.black)
+                            .bold()
+                    }
+                }
+            }
+        }
+        .padding(.top)
     }
 }
 
