cc9605a15a5476d82ae6a8ccc893d88c796ef1d5
Martin Martinez <mtz.martin@proton.me>
2023-11-13 16:56:40 -0600

    Yeah, is better with separated files

--- Podcast/HorizontalCard.swift
+++ Podcast/HorizontalCard.swift
@@ -9,7 +9,47 @@ import SwiftUI
 
 struct HorizontalCard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 25.0)
+            .frame(width: 360, height: 200)
+            .foregroundColor(.indigo)
+            .overlay(
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("ENJOY TOP PODCAST")
+                            
+                        Text("Listen Favourite\nPodcast")
+                            .padding(.top)
+                            .bold()
+                            .font(.title2)
+                        
+                        Button {
+                            //
+                        } label: {
+                            RoundedRectangle(cornerRadius: 25.0)
+                                .frame(width: 110, height: 30)
+                                .foregroundColor(Color(red: 144/255, green: 238/255, blue: 144/255))
+                                .overlay(
+                                    HStack{
+                                        Image(systemName: "play.circle.fill")
+                                            .foregroundColor(.black)
+                                            .font(.title3)
+                                        Text("Play Now")
+                                            .foregroundColor(.black)
+                                            .font(.footnote)
+                                            .bold()
+                                    }
+                                )
+                        }
+                        .shadow(radius: 10)
+                            
+                    }.foregroundColor(.white)
+                    
+                    Image("fondo")
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .scaledToFit()
+                }
+            )
     }
 }
 
--- Podcast/MainView.swift
+++ Podcast/MainView.swift
@@ -19,51 +19,6 @@ struct MainView: View {
     }
 }
 
-struct HorizontalCard: View {
-    var body: some View {
-        RoundedRectangle(cornerRadius: 25.0)
-            .frame(width: 360, height: 200)
-            .foregroundColor(.indigo)
-            .overlay(
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("ENJOY TOP PODCAST")
-                            
-                        Text("Listen Favourite\nPodcast")
-                            .padding(.top)
-                            .bold()
-                            .font(.title2)
-                        
-                        Button {
-                            //
-                        } label: {
-                            RoundedRectangle(cornerRadius: 25.0)
-                                .frame(width: 110, height: 30)
-                                .foregroundColor(Color(red: 144/255, green: 238/255, blue: 144/255))
-                                .overlay(
-                                    HStack{
-                                        Image(systemName: "play.circle.fill")
-                                            .foregroundColor(.black)
-                                            .font(.title3)
-                                        Text("Play Now")
-                                            .foregroundColor(.black)
-                                            .font(.footnote)
-                                            .bold()
-                                    }
-                                )
-                        }
-                        .shadow(radius: 10)
-                            
-                    }.foregroundColor(.white)
-                    
-                    Image("fondo")
-                        .resizable()
-                        .frame(width: 150, height: 150)
-                        .scaledToFit()
-                }
-            )
-    }
-}
 
 struct CategoriesButtons: View {
     var body: some View {
